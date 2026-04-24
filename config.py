3bbe872b7148fb774e80f85c4dffbebcc04a696b
man-ella <kwawumanuella@gmail.com>
2026-04-24 11:32:01 -0400

    Update servo channel mapping

--- config.py
+++ config.py
@@ -84,9 +84,9 @@ class HardwareConfig:
         default_factory=lambda: {
             "base": ServoCalibration(channel=0, min_angle=10, max_angle=170, home_angle=90),
             "shoulder": ServoCalibration(channel=1, min_angle=15, max_angle=165, home_angle=90),
-            "elbow": ServoCalibration(channel=2, min_angle=15, max_angle=165, home_angle=90, invert=True),
-            "wrist": ServoCalibration(channel=3, min_angle=20, max_angle=160, home_angle=90),
-            "dispenser": ServoCalibration(channel=4, min_angle=70, max_angle=130, home_angle=90),
+            "elbow": ServoCalibration(channel=4, min_angle=15, max_angle=165, home_angle=90, invert=True),
+            "wrist": ServoCalibration(channel=5, min_angle=20, max_angle=160, home_angle=90),
+            "dispenser": ServoCalibration(channel=6, min_angle=70, max_angle=130, home_angle=90),
         }
     )
 
